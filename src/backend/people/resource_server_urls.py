32b13d0bca5291d69ee1d7a97aa42106ac2beb87
Quentin BEY <quentin.bey@mail.numerique.gouv.fr>
2025-04-30 14:14:27 +0200

    ✨(resource-server) add SCIM `/Me` endpoint
    This provide a "self-care" SCIM endpoint, authenticated with OIDC token
    introspection. This endpoint will be use by services to fetch the user's
    team list.
    
    We chose to use the SCIM format (even if this is not a SCIM context) to
    make it easier to understand/maintain/plug.

--- src/backend/people/resource_server_urls.py
+++ src/backend/people/resource_server_urls.py
@@ -3,18 +3,23 @@
 from django.urls import include, path
 
 from lasuite.oidc_resource_server.urls import urlpatterns as resource_server_urls
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from core.api.resource_server import viewsets
+from core.api.resource_server.scim import viewsets as scim_viewsets
 
 # - Main endpoints
 # Contacts will be added later
 # Users will be added later
-router = DefaultRouter()
+router = SimpleRouter()
 router.register("teams", viewsets.TeamViewSet, basename="teams")
 
+# - SCIM endpoints
+scim_router = SimpleRouter()
+scim_router.register("Me", scim_viewsets.MeViewSet, basename="scim-me")
+
 # - Routes nested under a team
-team_related_router = DefaultRouter()
+team_related_router = SimpleRouter()
 team_related_router.register(
     "invitations",
     viewsets.InvitationViewset,
@@ -33,6 +38,7 @@ urlpatterns = [
                 *router.urls,
                 *resource_server_urls,
                 path("teams/<uuid:team_id>/", include(team_related_router.urls)),
+                path("scim/", include(scim_router.urls)),
             ]
         ),
     ),
