737049359249a8ccb5dec4dbb2e5e92b5e86dfa0
Quentin BEY <quentin.bey@mail.numerique.gouv.fr>
2025-05-06 15:32:23 +0200

    🐛(frontend) fix browser language detection
    The `Default language` E2E test detected the browser language was not
    automatically detected when user does not have any cookie, it was always
    falling back on the defaut language (en).

--- src/frontend/apps/desk/src/i18n/initI18n.ts
+++ src/frontend/apps/desk/src/i18n/initI18n.ts
@@ -1,4 +1,5 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 import {
@@ -8,7 +9,11 @@ import {
 } from './conf';
 import resources from './translations.json';
 
+export const availableFrontendLanguages: readonly string[] =
+  Object.keys(resources);
+
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
@@ -23,7 +28,7 @@ i18n
     interpolation: {
       escapeValue: false,
     },
-    preload: Object.keys(LANGUAGES_ALLOWED),
+    preload: availableFrontendLanguages,
     nsSeparator: false,
     keySeparator: false,
   })
