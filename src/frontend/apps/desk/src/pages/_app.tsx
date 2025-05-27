23f0d51569c8eb85afc79ff825e455d84463f53e
Eléonore Voisin <elevoisin@gmail.com>
2025-05-27 17:34:45 +0200

    ✨(frontend) add crisp script
    add crisp chatbox to global layout

--- src/frontend/apps/desk/src/pages/_app.tsx
+++ src/frontend/apps/desk/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { AppProvider } from '@/core/';
@@ -11,9 +12,29 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+declare global {
+  interface Window {
+    $crisp?: Array<[]>;
+    CRISP_WEBSITE_ID?: string;
+  }
+}
+
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
   const { t } = useTranslation();
+
+  useEffect(() => {
+    if (!window.$crisp) {
+      window.$crisp = [];
+      window.CRISP_WEBSITE_ID = '58ea6697-8eba-4492-bc59-ad6562585041';
+
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = true;
+      script.src = 'https://client.crisp.chat/l.js';
+      document.head.appendChild(script);
+    }
+  }, []);
 
   return (
     <>
