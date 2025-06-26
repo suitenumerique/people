09f448ce760988e6882db2b07df02302cd496c7b
Marie PUPO JEAMMET <marie.jeammet@beta.gouv.fr>
2025-06-26 17:47:30 +0200

    🐛(webhook) search existing Matrix user before inviting
    fix wrong formatting of user id + now searches for existing Matrix
    account of the user before inviting them to webhook room.

--- src/backend/core/tests/utils/test_webhooks_matrix_client.py
+++ src/backend/core/tests/utils/test_webhooks_matrix_client.py
@@ -13,9 +13,61 @@ from rest_framework import status
 from core import factories
 from core.enums import WebhookProtocolChoices
 from core.tests.fixtures import matrix
+from core.utils.matrix import MatrixAPIClient
 from core.utils.webhooks import webhooks_synchronizer
 
 pytestmark = pytest.mark.django_db
+
+
+## SEARCH
+@responses.activate
+def test_matrix_webhook__search_user_unknown(caplog):
+    """When searching for a user in the Matrix federation but cannot find any,
+    we invite a (future ?) user using user's email and room's server."""
+    caplog.set_level(logging.INFO)
+
+    user = factories.UserFactory()
+    webhook = factories.TeamWebhookFactory(
+        protocol=WebhookProtocolChoices.MATRIX,
+        url="https://www.matrix.org/#/room/room_id:room_server.au",
+        secret="secret-access-token",
+    )
+
+    client = MatrixAPIClient()
+    # Mock successful responses
+    responses.post(
+        re.compile(r".*/search"),
+        body=json.dumps(matrix.mock_search_empty()["message"]),
+        status=status.HTTP_200_OK,
+        content_type="application/json",
+    )
+    response = client.get_user_id(user=user, webhook=webhook)
+    assert response == f"@{user.email.replace('@', '-')}:room_server.au"
+
+
+@responses.activate
+def test_matrix_webhook__search_multiple_ids(caplog):
+    """When searching for a user in Matrix federation,
+    if user directory returns multiple ids, invite the first one."""
+    caplog.set_level(logging.INFO)
+
+    user = factories.UserFactory()
+    webhook = factories.TeamWebhookFactory(
+        protocol=WebhookProtocolChoices.MATRIX,
+        url="https://www.matrix.org/#/room/room_id:room_server.au",
+        secret="secret-access-token",
+    )
+
+    client = MatrixAPIClient()
+    # Mock successful responses
+    responses.post(
+        re.compile(r".*/search"),
+        body=json.dumps(matrix.mock_search_successful_multiple(user)["message"]),
+        status=status.HTTP_200_OK,
+        content_type="application/json",
+    )
+    response = client.get_user_id(user=user, webhook=webhook)
+    assert response == f"@{user.email.replace('@', '-')}:user_server1.com"
 
 
 ## INVITE
@@ -37,6 +89,11 @@ def test_matrix_webhook__invite_user_to_room_forbidden(caplog):
         re.compile(r".*/join"),
         body=str(matrix.mock_join_room_successful),
         status=status.HTTP_200_OK,
+    )
+    responses.post(
+        re.compile(r".*/search"),
+        body=json.dumps(matrix.mock_search_successful(user)["message"]),
+        status=matrix.mock_search_successful(user)["status_code"],
     )
     responses.post(
         re.compile(r".*/invite"),
@@ -63,6 +120,11 @@ def test_matrix_webhook__invite_user_to_room_already_in_room(caplog):
         re.compile(r".*/join"),
         body=str(matrix.mock_join_room_successful("room_id")["message"]),
         status=matrix.mock_join_room_successful("room_id")["status_code"],
+    )
+    responses.post(
+        re.compile(r".*/search"),
+        body=json.dumps(matrix.mock_search_successful(user)["message"]),
+        status=matrix.mock_search_successful(user)["status_code"],
     )
     responses.post(
         re.compile(r".*/invite"),
@@ -102,6 +164,11 @@ def test_matrix_webhook__invite_user_to_room_success(caplog):
         status=matrix.mock_join_room_successful("room_id")["status_code"],
     )
     responses.post(
+        re.compile(r".*/search"),
+        body=json.dumps(matrix.mock_search_successful(user)["message"]),
+        status=matrix.mock_search_successful(user)["status_code"],
+    )
+    responses.post(
         re.compile(r".*/invite"),
         body=str(matrix.mock_invite_successful()["message"]),
         status=matrix.mock_invite_successful()["status_code"],
@@ -113,8 +180,8 @@ def test_matrix_webhook__invite_user_to_room_success(caplog):
     assert webhook.secret in headers["Authorization"]
 
     # Check payloads sent to Matrix API
-    assert json.loads(responses.calls[1].request.body) == {
-        "user_id": f"@{user.email.replace('@', ':')}",
+    assert json.loads(responses.calls[2].request.body) == {
+        "user_id": f"@{user.email.replace('@', '-')}:user_server.com",
         "reason": f"User added to team {webhook.team} on People",
     }
 
@@ -148,6 +215,11 @@ def test_matrix_webhook__override_secret_for_tchap():
         status=matrix.mock_join_room_successful("room_id")["status_code"],
     )
     responses.post(
+        re.compile(r".*/search"),
+        body=json.dumps(matrix.mock_search_successful(user)["message"]),
+        status=matrix.mock_search_successful(user)["status_code"],
+    )
+    responses.post(
         re.compile(r".*/invite"),
         body=str(matrix.mock_invite_successful()["message"]),
         status=matrix.mock_invite_successful()["status_code"],
@@ -179,6 +251,11 @@ def test_matrix_webhook__kick_user_from_room_not_in_room(caplog):
         status=status.HTTP_200_OK,
     )
     responses.post(
+        re.compile(r".*/search"),
+        body=json.dumps(matrix.mock_search_successful(user)["message"]),
+        status=matrix.mock_search_successful(user)["status_code"],
+    )
+    responses.post(
         re.compile(r".*/kick"),
         body=str(matrix.mock_kick_user_not_in_room()["message"]),
         status=matrix.mock_kick_user_not_in_room()["status_code"],
@@ -205,7 +282,7 @@ def test_matrix_webhook__kick_user_from_room_success(caplog):
     user = factories.UserFactory()
     webhook = factories.TeamWebhookFactory(
         protocol=WebhookProtocolChoices.MATRIX,
-        url="https://www.matrix.org/#/room/room_id:home_server",
+        url="https://www.matrix.org/#/room/room_id:room_server",
         secret="secret-access-token",
     )
 
@@ -215,6 +292,11 @@ def test_matrix_webhook__kick_user_from_room_success(caplog):
         status=status.HTTP_200_OK,
     )
     responses.post(
+        re.compile(r".*/search"),
+        body=json.dumps(matrix.mock_search_successful(user)["message"]),
+        status=matrix.mock_search_successful(user)["status_code"],
+    )
+    responses.post(
         re.compile(r".*/kick"),
         body=str(matrix.mock_kick_successful),
         status=status.HTTP_200_OK,
@@ -222,8 +304,8 @@ def test_matrix_webhook__kick_user_from_room_success(caplog):
     webhooks_synchronizer.remove_user_from_group(team=webhook.team, user=user)
 
     # Check payloads sent to Matrix API
-    assert json.loads(responses.calls[1].request.body) == {
-        "user_id": f"@{user.email.replace('@', ':')}",
+    assert json.loads(responses.calls[2].request.body) == {
+        "user_id": f"@{user.email.replace('@', '-')}:user_server.com",
         "reason": f"User removed from team {webhook.team} on People",
     }
 
@@ -257,6 +339,11 @@ def test_matrix_webhook__kick_user_from_room_forbidden(caplog):
         re.compile(r".*/join"),
         body=str(matrix.mock_join_room_successful),
         status=status.HTTP_200_OK,
+    )
+    responses.post(
+        re.compile(r".*/search"),
+        body=json.dumps(matrix.mock_search_successful(user)["message"]),
+        status=matrix.mock_search_successful(user)["status_code"],
     )
     responses.post(
         re.compile(r".*/kick"),
