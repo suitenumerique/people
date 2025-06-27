709628a26a09f9dd3e4f68265ef34b3d55621353
Manuel Raynaud <manu@raynaud.io>
2025-06-27 15:18:27 +0200

    🔧(back) remove usage of deprecated db engine (#937)
    The db engine postgresql_psycopg2 does not exists anymore in django but
    for BC compat it is possible to use it in the configuration and it is
    replace by postgresql at runtime. We changed this settings to use the
    good one.

--- src/backend/people/settings.py
+++ src/backend/people/settings.py
@@ -102,7 +102,7 @@ class Base(Configuration):
     DATABASES = {
         "default": {
             "ENGINE": values.Value(
-                "django.db.backends.postgresql_psycopg2",
+                "django.db.backends.postgresql",
                 environ_name="DB_ENGINE",
                 environ_prefix=None,
             ),
