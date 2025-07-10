9b47b289da2e0f2603af2487db1a7e29a29811da
Marie PUPO JEAMMET <marie.jeammet@beta.gouv.fr>
2025-07-10 12:30:07 +0200

    fixup! ✨(api) update mailboxes

--- src/backend/mailbox_manager/api/permissions.py
+++ src/backend/mailbox_manager/api/permissions.py
@@ -14,18 +14,13 @@ class AccessPermission(core_permissions.IsAuthenticated):
         return abilities.get(request.method.lower(), False)
 
 
-class MailBoxPermission(core_permissions.IsAuthenticated):
+class MailBoxPermission(AccessPermission):
     """Permission class to manage mailboxes for a mail domain"""
 
     def has_permission(self, request, view):
         """Check permission based on domain."""
         domain = models.MailDomain.objects.get(slug=view.kwargs.get("domain_slug", ""))
         abilities = domain.get_abilities(request.user)
-        return abilities.get(request.method.lower(), False)
-
-    def has_object_permission(self, request, view, obj):
-        """Check permission for a given object."""
-        abilities = obj.get_abilities(request.user)
         return abilities.get(request.method.lower(), False)
 
 
